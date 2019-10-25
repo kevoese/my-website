c0d9957b7380520ed0b4bff3bf74b73c830626dc
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-10-25 20:02:59 +0100

    Setup react and hosting environment

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './pages/Main';
+import './style/app.scss';
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-undef */
+require('dotenv').config();
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+let isDevelopment = false;
+
+if (['staging', 'review', 'production'].includes(process.env.NODE_ENV)) {
+  isDevelopment = false;
+} else isDevelopment = true;
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  entry: path.join(__dirname, 'src', 'App.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            sourceMap: isDevelopment,
+          },
+        },
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|otf|ttf|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    watchContentBase: true,
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+  },
+};
