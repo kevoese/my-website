9415a818c5e75902c1096996c99c644ca09f7838
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-10-28 14:05:42 +0100

    complete mail setup

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
+/* eslint-disable no-console */
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const controller = require('./mailer');
 
 dotenv.config();
 const port = process.env.PORT || 7000;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post('/sendMail', controller.validateRequest, controller.mailer);
+
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+});
+
 app.listen(port, () => {
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
-  });
+  console.log(`app is running on port ${port}`);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -7,82 +7,88 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-let isDevelopment = false;
+// let isDevelopment = false;
 
-if (['staging', 'review', 'production'].includes(process.env.NODE_ENV)) {
-  isDevelopment = false;
-} else isDevelopment = true;
+// if (['staging', 'review', 'production'].includes(process.env.NODE_ENV)) {
+//   isDevelopment = false;
+// } else isDevelopment = true;
 
-module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
-  entry: path.join(__dirname, 'src', 'App.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            sourceMap: isDevelopment,
-          },
-        },
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
+module.exports = (env, argv) => {
+  let isDevelopment = false;
+  if (['staging', 'review', 'production'].includes(argv.mode)) {
+    isDevelopment = false;
+  } else isDevelopment = true;
+
+  return {
+    entry: path.join(__dirname, 'src', 'App.js'),
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'app.bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
               sourceMap: isDevelopment,
             },
           },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|otf|ttf|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {},
-          },
-        ],
-      },
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|otf|ttf|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {},
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss'],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      watchContentBase: true,
+    },
+    plugins: [
+      new Dotenv(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html'),
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss'],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    watchContentBase: true,
-  },
-  plugins: [
-    new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
-    }),
-  ],
-  optimization: {
-    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
-  },
+    optimization: {
+      minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+    },
+  };
 };
